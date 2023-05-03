68b450dee1464d18a69b6b0b7a392d91d3f4c9e6
sathish1980 <kumar.sathish189@gmail.com>
2023-05-03 08:20:13 +0530

    new sample file added

---
+++ src/main/java/atm/SeleniumTambaram/sample.java
@@ -0,0 +1,15 @@
+package atm.SeleniumTambaram;
+
+public class sample {
+	
+	public void sample1()
+	{
+		System.out.println("git");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
