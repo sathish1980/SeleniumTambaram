4808b0805ee4652dca9ec68784104d6490627c8f
sathish1980 <kumar.sathish189@gmail.com>
2023-05-04 07:47:48 +0530

    new changes

--- src/main/java/atm/SeleniumTambaram/sample.java
+++ src/main/java/atm/SeleniumTambaram/sample.java
@@ -7,6 +7,12 @@ public class sample {
 		System.out.println("git");
 	}
 
+	public void mergeconflicts()
+	{
+		System.out.println("mergeconflicts");
+	}
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
